768d4fdc25cc9872cd8c2a09d00bdbb3d8a82f71
JohanBach5 <johan.bach@epfl.ch>
2026-04-14 12:03:04 +0200

    Modified main.py so that it creates the plot and prints the spike times

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import LIF
+
 matplotlib.use('TkAgg')
+
+
+neuron = LIF.LIF()
+input_current = 2.0
+
+for t in range(500):
+    neuron.step(input_current, t)
+
+plt.plot(neuron.times, neuron.voltages)
+plt.axhline(y=neuron.threshold, color='orange', linestyle='--', label='threshold')
+plt.xlabel('Time (ms)')
+plt.ylabel('Vm (mV)')
+plt.legend()
+plt.savefig('LIF.png')
+
+print(neuron.spike_times)
